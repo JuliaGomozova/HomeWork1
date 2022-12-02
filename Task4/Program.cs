d3ec29242252631eb6493322eceeee8370234d9e
Julia Gomozova <sunrain@yandex.ru>
2022-12-02 17:54:28 +0300

    add Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число больше нуля:");
+string numStr = Console.ReadLine() ??"";
+
+int num = Convert.ToInt32(numStr);
+if (num <= 0 )
+{
+    Console.WriteLine("Число должно быть больше нуля");
+}
+else
+{
+    Console.WriteLine($"Четные числа от 1 до {num}:");
+    for (int i = 1; i <= num; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
+
+
