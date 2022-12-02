7966651683195fd19733d60b8a36198641749ff5
Julia Gomozova <sunrain@yandex.ru>
2022-12-02 13:21:49 +0300

    add Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите первое число:");
+string numStr1 = Console.ReadLine() ??"";
+
+Console.WriteLine("введите второе число:");
+string numStr2 = Console.ReadLine() ??""; 
+
+int num1 = Convert.ToInt32(numStr1);
+int num2 = Convert.ToInt32(numStr2);
+
+    Console.WriteLine("эти числа равны"); 
+    if (num1>num2) 
+    {
+        Console.WriteLine($"число {num1} больше числа {num2}");
+    }
+    else if (num2>num1)
+    { 
+        Console.WriteLine ($"число {num2} больше числа {num1}");
+    }
+    else Console.WriteLine($"число {num1} равно числу {num2}");
