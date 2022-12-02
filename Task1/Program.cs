a639afdafdfb8deadf8adc1fe496873d8a9ca252
Julia Gomozova <sunrain@yandex.ru>
2022-12-02 13:25:02 +0300

    fix error

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -7,12 +7,12 @@ string numStr2 = Console.ReadLine() ??"";
 int num1 = Convert.ToInt32(numStr1);
 int num2 = Convert.ToInt32(numStr2);
 
-    if (num1>num2) 
-    {
-        Console.WriteLine($"число {num1} больше числа {num2}");
-    }
-    else if (num2>num1)
-    { 
-        Console.WriteLine ($"число {num2} больше числа {num1}");
-    }
-    else Console.WriteLine($"число {num1} равно числу {num2}");
+if (num1>num2) 
+{
+     Console.WriteLine($"число {num1} больше числа {num2}");
+}
+else if (num2>num1)
+{ 
+    Console.WriteLine ($"число {num2} больше числа {num1}");
+}
+else Console.WriteLine($"число {num1} равно числу {num2}");
