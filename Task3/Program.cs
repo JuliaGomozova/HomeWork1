bd6bd3598c50d07f4b9ca489d214d49460d88af4
Julia Gomozova <sunrain@yandex.ru>
2022-12-02 17:23:11 +0300

    add Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+string numStr = Console.ReadLine() ??"";
+
+int num = Convert.ToInt32(numStr);
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else Console.WriteLine("Число нечетное");
